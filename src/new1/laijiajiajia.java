d4a260ab2f31f3df0b87d8da680de9f68aaceff3
RankeGamer <951557662@qq.com>
2021-03-20 23:16:14 +0800

    星号阵列

---
+++ src/new1/laijiajiajia.java
@@ -0,0 +1,12 @@
+package new1;
+public class laijiajiajia {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=10) {			
+			int j=10*i++;
+			System.out.print(j+" ");
+			System.out.println(i);
+		}
+	}
+
+}
