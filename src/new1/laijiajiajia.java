45ccd7dc18874d94760f320b338d295fd46dc03e
RankeGamer <951557662@qq.com>
2021-03-22 19:17:18 +0800

    leisile+2

--- src/new1/laijiajiajia.java
+++ src/new1/laijiajiajia.java
@@ -3,14 +3,18 @@ public class laijiajiajia {
 	public static void main(String[] args) {
 		int i=5;
 		int j=i%10;
+		String[] ave= {"1","5","8"};		
 		int k=11/2;
-		String a="999";
-		char b= a.charAt(1);
-		String d=b+"";
-		 int c=Integer.parseInt(b+"");
-		System.out.print(a.charAt(1));
-		System.out.println(k);
-
+		String a=ave[1];
+		String c=ave[2];
+		//char b= a.charAt(1);
+		//String d=b+"";
+		 //int c=Integer.parseInt(b+"");
+		//System.out.print(a.charAt(1));
+		int b= Integer.parseInt(a);
+		System.out.println(b);
+		System.out.println(c);
+ 
 	}
 
 }
--- src/xunhuan5/Jisuanfangcha545.java
+++ src/xunhuan5/Jisuanfangcha545.java
@@ -4,7 +4,24 @@ public class Jisuanfangcha545 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		java.util.Scanner input=new java.util.Scanner(System.in);
+		System.out.print("请输入十个数字，以空格隔开");
+		String age=input.nextLine();		
+		String[] ages=age.split(" ");
+		//求平均年龄
+		double ave=0;
+		for (int i=0;i<10;i++) {
+			ave=ave+Double.parseDouble(ages[i]);
+		}
+		ave=ave/10;
+		System.out.printf("平均年龄为%.2f\n",ave);
+		//求年龄方差
+		double s=0;
+		for (int i=0;i<10;i++) {
+			s=s+Math.pow(ave-Double.parseDouble(ages[i]),2);					
+		}
+		s=Math.sqrt(s/9);	
+		System.out.printf("年龄标准差为%.5f",s);		
 	}
 
 }
